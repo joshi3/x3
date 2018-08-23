0d71a3edef85f1eeac5d14bb2047c2c1b7089056
joshi3 <joshi.naga3@gmail.com>
2018-08-23 23:42:17 +0530

    FP : Use lineage fingerprint hal to support legacy devices Signed-off-by: joshi3 <joshi.naga3@gmail.com>

--- hidl.mk
+++ hidl.mk
@@ -93,8 +93,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-	android.hardware.biometrics.fingerprint@2.1-impl \
-    android.hardware.biometrics.fingerprint@2.1-service
+	android.hardware.biometrics.fingerprint@2.0-service
 
 # OMX
 PRODUCT_PACKAGES += \
