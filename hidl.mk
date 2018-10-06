307f8d3f188d651a3cb7cee093a90c954f417b87
joshi3 <joshi.naga3@gmail.com>
2018-10-06 23:15:46 +0530

    Change camera hal to legacy Signed-off-by: joshi3 <joshi.naga3@gmail.com>

--- hidl.mk
+++ hidl.mk
@@ -39,10 +39,8 @@ PRODUCT_PACKAGES += \
 
 # Camera HAL
 PRODUCT_PACKAGES += \
-    camera.device@1.0-impl \
-    camera.device@3.2-impl \
-    android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service
+    camera.device@1.0-impl-legacy \
+    android.hardware.camera.provider@2.4-impl-legacy
 
 # Bluetooth HAL
 PRODUCT_PACKAGES += \
